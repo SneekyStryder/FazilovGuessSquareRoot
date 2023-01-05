1b7055846239ffc6f958142ceaae36d73bd82236
Imran Fazilov <ifazilov3584@bths.edu>
2023-01-05 11:56:10 -0500

    finish later

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What number do you want to find the square root of: ");
+        double userNum = scan.nextDouble();
+        SquareRootGuesser guesser = new SquareRootGuesser(userNum);
+
+
+    }
 }
--- src/SquareRootGuesser.java
+++ src/SquareRootGuesser.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class SquareRootGuesser {
+public class SquareRootGuesser {
+    private double num;
+    private int bet1;
+    private int bet2;
+
+    public SquareRootGuesser(double num) {
+        this.num = num;
+    }
+
+    public int getBet1() {
+        return bet1;
+    }
+
+    public int getBet2() {
+        return bet2;
+    }
+
+    public void findBet1() {
+        int i = 0;
+        boolean notFound = true;
+        while (notFound) {
+            if ((Math.sqrt(num - i) * 10) % 10 == 0) {
+                bet1 = (int) Math.sqrt(num - i);
+                notFound = false;
+            }
+            else {
+                i++;
+            }
+        }
+    }
+
+    public void findBet2() {
+        int i = 0;
+        boolean notFound = true;
+        while (notFound) {
+            if ((Math.sqrt(num + i) * 10) % 10 == 0) {
+                bet2 = (int) Math.sqrt(num + i);
+                notFound = false;
+            }
+            else {
+                i++;
+            }
+        }
+    }
+
 }
