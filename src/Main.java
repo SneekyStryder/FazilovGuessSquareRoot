f8319262a961df95b66f94f2f30943e839271dcc
Sneeky Stryder <ifazilov3584@bths.edu>
2023-01-05 21:09:54 -0500

    what the freak

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,12 @@ public class Main {
         System.out.print("What number do you want to find the square root of: ");
         double userNum = scan.nextDouble();
         SquareRootGuesser guesser = new SquareRootGuesser(userNum);
-
+        guesser.findBet1();
+        guesser.findBet2();
+        System.out.println("sqrt(" + (double) userNum + ") is between " + guesser.getBet1() + " and " + guesser.getBet2());
+        System.out.print("How close would you like the estimate to be: ");
+        double threshold = scan.nextDouble();
+        System.out.println("Square root of " + (double) userNum + " is about " + guesser.compGuess(threshold));
 
     }
 }
--- src/SquareRootGuesser.java
+++ src/SquareRootGuesser.java
@@ -1,26 +1,28 @@
 public class SquareRootGuesser {
     private double num;
-    private int bet1;
-    private int bet2;
+    private double bet1;
+    private double bet2;
+    private double answer;
 
     public SquareRootGuesser(double num) {
         this.num = num;
+        answer = Math.sqrt(num);
     }
 
-    public int getBet1() {
+    public double getBet1() {
         return bet1;
     }
 
-    public int getBet2() {
+    public double getBet2() {
         return bet2;
     }
 
     public void findBet1() {
-        int i = 0;
+        int i = 1;
         boolean notFound = true;
         while (notFound) {
             if ((Math.sqrt(num - i) * 10) % 10 == 0) {
-                bet1 = (int) Math.sqrt(num - i);
+                bet1 = Math.sqrt(num - i);
                 notFound = false;
             }
             else {
@@ -30,17 +32,47 @@ public class SquareRootGuesser {
     }
 
     public void findBet2() {
-        int i = 0;
+        int i = 1;
         boolean notFound = true;
         while (notFound) {
             if ((Math.sqrt(num + i) * 10) % 10 == 0) {
-                bet2 = (int) Math.sqrt(num + i);
+                bet2 = Math.sqrt(num + i);
                 notFound = false;
             }
             else {
                 i++;
             }
         }
+    }
+
+    public double compGuess(double threshold) {
+        double thresholdAnswer = answer;
+        int counter = 0;
+        double tempThreshold = threshold;
+        while (tempThreshold != 1) {
+            tempThreshold = tempThreshold * 10;
+            counter++;
+        }
+        for (int i = 1; i <= counter; i++) {
+            thresholdAnswer = thresholdAnswer * 10;
+        }
+        thresholdAnswer = (int) thresholdAnswer;
+        for (int i = 1; i <= counter; i++) {
+            thresholdAnswer = thresholdAnswer / 10;
+        }
+        double limit1 = bet1;
+        double limit2 = bet2;
+        double guess = (limit1 + limit2) / 2;
+        double guessSquared = guess * guess;
+        while (((((int) (guessSquared * (Math.pow(10, counter)))) / (Math.pow(10, counter))) != thresholdAnswer + threshold) || (guessSquared != thresholdAnswer - threshold)) {
+            if (guessSquared > answer) {
+                guessSquared -= threshold;
+            }
+            else if (guessSquared < answer) {
+                guessSquared += threshold;
+            }
+        }
+        return guessSquared;
     }
 
 }
